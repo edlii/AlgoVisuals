4bf67522a7580a834a2366c099e764b72909b172
ColdSashimi <76571219+ColdSashimi@users.noreply.github.com>
2022-02-22 18:37:29 -0800

    A* now finds the path

--- algorithms/astar.py
+++ algorithms/astar.py
@@ -6,79 +6,90 @@ from ui.cell_grid import getTarget, searchPressed, getStart
 
 def astar(grid, timeTick):
     searchPressed()
-    start_cell = getStart()
-    start_cell.position = (start_cell.getX(), start_cell.getY())
-    start_cell.parent = None
-    start_cell.g = start_cell.h = start_cell.f = 0
-    end_cell = getTarget()
-    end_cell.position = (end_cell.getX(), end_cell.getY())
-    end_cell.parent = None
+    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+    # Create start and end node
+    start_node = getStart()
+    start_node.position = (getStart().getX(), getStart().getY())
+    start_node.parent = None
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = getTarget()
+    end_node.position = (getTarget().getX(), getTarget().getY())
+    end_node.parent = None
+    end_node.g = end_node.h = end_node.f = 0
 
-    # Initialize open and closed list
+    # Initialize both open and closed list
     open_list = []
     closed_list = []
 
-    # Append starting node
-    open_list.append(start_cell)
+    # Add the start node
+    open_list.append(start_node)
 
+    # Loop until you find the end
     while len(open_list) > 0:
-        current_cell = open_list[0]
+        # Get the current node
+        current_node = open_list[0]
         current_index = 0
         for index, item in enumerate(open_list):
-            if item.f < current_cell.f:
-                current_cell = item
+            if item.f < current_node.f:
+                current_node = item
                 current_index = index
 
+        # Pop current off open list, add to closed list
         open_list.pop(current_index)
-        closed_list.append(current_cell)
+        closed_list.append(current_node)
 
-        if current_cell == end_cell:
+        # Found the goal
+        if current_node == end_node:
             path = []
-            current = current_cell
+            current = current_node
             while current is not None:
                 path.append(current.position)
                 current = current.parent
-            grid.drawPath(path[::-1])
+            grid.drawPath(path[::-1]) # Return reversed path
             return
 
+        # Generate children
         children = []
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
-            cell_position = (current_cell.position[0] + new_position[0], current_cell.position[1] + new_position[1])
-        
-            if cell_position[0] > (len(grid.grid) - 1) or cell_position[0] < 0 or cell_position[1] > (len(grid.grid[len(grid.grid)-1]) -1) or cell_position[1] < 0:
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(grid.grid) - 1) or node_position[0] < 0 or node_position[1] > (len(grid.grid[len(grid.grid)-1]) -1) or node_position[1] < 0:
                 continue
 
-            if grid.coords(cell_position[0], cell_position[1]).getType() == 3:
+            # Make sure walkable terrain
+            if grid.coords(node_position[1],node_position[0]).getType() == 3 or grid.coords(node_position[1],node_position[0]).getType() == 1:
                 continue
 
-            new_cell = grid.coords(cell_position[0], cell_position[1])
-            new_cell.position = (cell_position[0], cell_position[1])
-            new_cell.parent = current_cell
+            # Create new node
+            new_node = grid.coords(node_position[1], node_position[0])
+            new_node.position = (node_position[0], node_position[1])
+            new_node.parent = current_node
 
-            if current_cell.parent != None:
-                while new_cell == current_cell.parent:
-                    if current_cell.parent != None or new_cell != None:
-                        break
-                    continue
+            # Append
+            children.append(new_node)
+            new_node.draw()
 
-            children.append(new_cell)
-            new_cell.draw()
-
-        
+        # Loop through children
         for child in children:
+
+            # Child is on the closed list
             for closed_child in closed_list:
                 if child == closed_child:
                     continue
-            children = list(dict.fromkeys(children))
-            print("At: " + "("+ str(child.getX()) + ", " + str(child.getY()) + ")")
-            
-            child.g = current_cell.g + 1
-            child.h = ((child.position[0] - end_cell.position[0]) ** 2) + ((child.position[1] - end_cell.position[1]) ** 2)
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
             child.f = child.g + child.h
 
-            for open_cell in open_list:
-                if child == open_cell and child.g > open_cell.g:
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
                     continue
 
+            # Add the child to the open list
             open_list.append(child)
         time.sleep(timeTick)
