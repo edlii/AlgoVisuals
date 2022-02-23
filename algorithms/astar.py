b9932c551b3da976cc1df0817dfcc3a9ed7c1d60
ColdSashimi <76571219+ColdSashimi@users.noreply.github.com>
2022-02-23 11:42:29 -0800

    A* mostly working now, barricade causing some bugs still

--- algorithms/astar.py
+++ algorithms/astar.py
@@ -20,6 +20,7 @@ def astar(grid, timeTick):
     # Initialize both open and closed list
     open_list = []
     closed_list = []
+    parents = []
 
     # Add the start node
     open_list.append(start_node)
@@ -28,7 +29,11 @@ def astar(grid, timeTick):
     while len(open_list) > 0:
         # Get the current node
         current_node = open_list[0]
+        if len(open_list) > 1:
+            open_list.pop(0)
+        # print(current_node.position)
         current_index = 0
+        open_list = list(dict.fromkeys(open_list))
         for index, item in enumerate(open_list):
             if item.f < current_node.f:
                 current_node = item
@@ -39,13 +44,17 @@ def astar(grid, timeTick):
         closed_list.append(current_node)
 
         # Found the goal
-        if current_node == end_node:
+        if (current_node.getX(), current_node.getY() + 1) == end_node.position:
             path = []
             current = current_node
             while current is not None:
-                path.append(current.position)
+                path.append((current.position[1], current.position[0]))
                 current = current.parent
-            grid.drawPath(path[::-1]) # Return reversed path
+            path = path[::-1]
+            # Removes extra path node occurring when prev. node has the same X value as the target X
+            if path[-2][1] != end_node.position[0]:
+                path.pop(-1)
+            grid.drawPath(path) # Return reversed path
             return
 
         # Generate children
@@ -56,17 +65,20 @@ def astar(grid, timeTick):
             node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
             # Make sure within range
-            if node_position[0] > (len(grid.grid) - 1) or node_position[0] < 0 or node_position[1] > (len(grid.grid[len(grid.grid)-1]) -1) or node_position[1] < 0:
+            if node_position[1] > (len(grid.grid) - 1) or node_position[1] < 0 or node_position[0] > (len(grid.grid[len(grid.grid)-1]) -1) or node_position[0] < 0:
                 continue
 
             # Make sure walkable terrain
-            if grid.coords(node_position[1],node_position[0]).getType() == 3 or grid.coords(node_position[1],node_position[0]).getType() == 1:
+            if grid.coords(node_position[1],node_position[0]).getType() == 3 or grid.coords(node_position[1],node_position[0]).getType() == 1 or grid.coords(node_position[1], node_position[0]).getType() == 2:
                 continue
 
             # Create new node
             new_node = grid.coords(node_position[1], node_position[0])
             new_node.position = (node_position[0], node_position[1])
-            new_node.parent = current_node
+            # Ensures parents don't loop into each other
+            if new_node.parent not in parents:
+                new_node.parent = current_node
+                parents.append(current_node)
 
             # Append
             children.append(new_node)
@@ -74,7 +86,6 @@ def astar(grid, timeTick):
 
         # Loop through children
         for child in children:
-
             # Child is on the closed list
             for closed_child in closed_list:
                 if child == closed_child:
